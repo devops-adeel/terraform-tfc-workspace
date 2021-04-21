e0e300047bcd0a82ecfef79645681008d179e252
tfc_run <team-is@hashicorp.com>
2021-04-21 16:10:50 +0100

    Rendered provider configuration

--- integration_tests/fixtures/provider.tf
+++ integration_tests/fixtures/provider.tf
@@ -3,9 +3,6 @@ terraform {
     vault = {
       source = "hashicorp/vault"
     }
-    tfe = {
-      version = "~> 0.24.0"
-    }
   }
   backend "remote" {
     organization = "hc-implementation-services"
@@ -22,7 +19,7 @@ variable "approle_secret" {}
 provider "vault" {
   auth_login {
     namespace = "admin/terraform-tfc-workspace"
-    path      = "auth/approle/login"
+    path = "auth/approle/login"
 
     parameters = {
       role_id   = var.approle_id
@@ -31,9 +28,3 @@ provider "vault" {
   }
 }
 
-locals {
-  application_name = "terraform-modules-development-tfc"
-  namespace        = "admin/terraform-tfc-workspace"
-  env              = "dev"
-  service          = "web"
-}
